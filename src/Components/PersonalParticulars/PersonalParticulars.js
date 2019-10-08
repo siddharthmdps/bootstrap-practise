d7ef50df3b8724c7e92dae219c2bd7060999b3ad
Siddharth SIngh <siddharthmdps@gmail.com>
2019-10-08 17:50:13 +0800

    Added Accordiance

---
+++ src/Components/PersonalParticulars/PersonalParticulars.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Form, Row, Col, Container} from 'react-bootstrap';
+
+const personalParticulars = props => {
+    return(
+        <React.Fragment>
+        <Form>
+            <Form.Group as={Row} controlId="formPlaintextEmail">
+                <Form.Label column sm="2">
+                Email
+                </Form.Label>
+                <Col sm="10">
+                <Form.Control plaintext readOnly defaultValue="email@example.com" />
+                </Col>
+            </Form.Group>
+
+            <Form.Group as={Row} controlId="formPlaintextPassword">
+                <Form.Label column sm="2">
+                Password
+                </Form.Label>
+                <Col sm="10">
+                <Form.Control type="password" placeholder="Password" />
+                </Col>
+            </Form.Group>
+        </Form>
+        </React.Fragment>
+    );
+}
+
+export default personalParticulars;
--- src/Container/Profile/Profile.js
+++ src/Container/Profile/Profile.js
@@ -1,13 +1,33 @@
 import React, {Component} from 'react';
 import classes from './Profile.module.css';
-
+import PersonalParticulars from '../../Components/PersonalParticulars/PersonalParticulars';
+import {Accordion, Card, Button, Container} from 'react-bootstrap';
 class Profile extends Component {
     render(){
         return(
-            <React.Fragment>
-                P
-            </React.Fragment>
-
+            <Container>
+                <Accordion defaultActiveKey="0">
+                <Card>
+                    <Accordion.Toggle as={Card.Header} eventKey="0">
+                    Personal Particulars
+                    </Accordion.Toggle>
+                    <Accordion.Collapse eventKey="0">
+                    <Card.Body>
+                        <PersonalParticulars />
+                    </Card.Body>
+                    </Accordion.Collapse>
+                </Card>
+                <Card>
+                    <Accordion.Toggle as={Card.Header} eventKey="1">
+                    Click me!
+                    </Accordion.Toggle>
+                    <Accordion.Collapse eventKey="1">
+                    <Card.Body>Hello! I'm another body</Card.Body>
+                    </Accordion.Collapse>
+                </Card>
+                </Accordion>
+            
+            </Container>
         );
     }
 }
